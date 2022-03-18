e40274092036663c42e3cdeae2a0d516646f0b69
Florian-Audouard <nairolf.audouard@gmail.com>
2022-03-19 01:09:30 +1100

    ajout commentaire et fichier json de test

--- cartes-logiques/src/components/Game.js
+++ cartes-logiques/src/components/Game.js
@@ -12,6 +12,14 @@ class CardClass {
     this.left = left;
     this.right = right;
   }
+  /**
+   *  renvoie sous la forme : exemple couleur : red, liaison possible : "^" et "=>"
+   * carte simple : (couleur)
+   * carte double : (couleur) liaison (couleur)
+   * carte triple : (couleur) liaison ((couleur) laision (couleur))
+   * carte quadruple : ((couleur) laision (couleur)) laision ((couleur) laision (couleur))
+   * @returns un string plus lisible
+   */
   toString() {
     var res = "(";
     if (this.color != null) {
@@ -32,6 +40,41 @@ class CardClass {
 
     return res + ")";
   }
+  /**
+   * renvoie un objet que l'on va stocker dans un fichier json
+   * voci un exemple :
+   * {color : "couleur"}
+   * { 
+   *   "left": { "color": "couleur" }, 
+   *   "liaison": num,
+   *   "right": { "color": "couleur" } 
+   * }
+   * { 
+   *   "left": { "color": "couleur" }, 
+   *   "liaison": num,
+   *   "right": { 
+   *              "left": { "color": "couleur" }, 
+   *              "liaison": num,
+   *              "right": { "color": "couleur" } 
+   *            }
+   * }
+   * { 
+   *   "left": { 
+   *              "left": { "color": "couleur" }, 
+   *              "liaison": num,
+   *              "right": { "color": "couleur" } 
+   *            },
+   *  "laision":num, 
+   *  "right": { 
+   *              "left": { "color": "couleur" }, 
+   *              "liaison": num,
+   *              "right": { "color": "couleur" } 
+   *            }
+   *
+   * }
+   * 
+   * @returns un onjet destiner au fichier json
+   */
   toFile() {
     if (this.color !== null) {
       return { color: this.color };
@@ -58,7 +101,9 @@ const Game = ({ mode }) => {
   const [indiceDeckAddCard, setIndiceDeckAddCard] = useState(0);
   const [popupFusion, setPopupFusion] = useState(false);
 
-
+  /**
+   * initialise l'exercice.
+   */
   useEffect(() => {
     
     if (mode !== "create") {
@@ -74,10 +119,7 @@ const Game = ({ mode }) => {
     }
   }, [mode]);
   /**
-   * @todo fonction qui affiche une fenetre pour que le joueur valide ou non la selection des 2 cartes et si oui effectue l'opreation associer
-   * possibiliter de changer le nom de la methode et les noms des attributs
-   * @param {*} c - index deck de la deuxieme carte selectionner
-   * @param {*} d - index carte de la deuxieme carte selectionner
+   * met la variable popupSelect en true ce qui affiche le popup.
    */
   const popup = () => {
     setPopupSelect(true);
@@ -178,11 +220,19 @@ const Game = ({ mode }) => {
     setPopupSelect(false);
     allFalse();
   };
-
+  /**
+   * fait apparaitre le popup qui nous demande la couleur de la carte qu'on veut ajouter
+   * @param {*} deckIndice - l'indice du deck ou on ajoute une carte 
+   */
   const addCard = (deckIndice) => {
     setIndiceDeckAddCard(deckIndice);
     setPopupAddCard(true);
   };
+  /**
+   *  creer une carte avec la couleur selectionner (ne ferme pas le popup quand on selectionne une couleur)
+   * @param {*} event (event.target.value)recois la couleur cliquer
+   *                  (event.target.checked)on le met a false si on veut faire plusieur fois la meme couleur
+   */
   const choixCouleur = (event) => {
     var tmp = [...game];
     event.target.checked = false;
@@ -198,6 +248,10 @@ const Game = ({ mode }) => {
     );
     setGame(tmp);
   };
+  /**
+   *  creer une carte complexe avec les 2 cartes selectionner (cette fonction est apeller a la fin de update en mode creation)
+   * @param {*} event (event.target.value)recois la liaison cliquer
+   */
   const choixLiaison = (event) => {
     console.log(game);
     var tmp = [...game];
@@ -220,6 +274,9 @@ const Game = ({ mode }) => {
     allFalse();
     console.log(game);
   };
+  /**
+   * suprime une carte la carte qui est selectionner
+   */
   const deleteCard = () => {
     setPopupDeleteCard(false);
     var tmp = [...game];
@@ -227,6 +284,11 @@ const Game = ({ mode }) => {
     setGame(tmp);
     allFalse();
   };
+  /**
+   * transforme le tableau en tableau d'objet avec seulement les information qui nous interesse (couleur/liaison)
+   * 
+   * @returns renvoie une tableau d'objet
+   */
   const gameOutput = () => {
     var res = [[], []];
     game.map(function (deck, index) {
@@ -239,6 +301,11 @@ const Game = ({ mode }) => {
 
     return res;
   };
+  /**
+   * recois un tableau d'un fichier json a qui on a apliquer la methode parse (json ==> tableau object) et renvoie un tableau qui peut etre lu par notre site
+   * @param {*} data tableau d'objet qui va servir pour l'initialisation
+   * @returns un tableau de deck
+   */
   const gameInput = (data) =>{
     var res = [[],[]]
     var i=0;
@@ -254,9 +321,19 @@ const Game = ({ mode }) => {
     console.log(res);
     return res;
   }
+  /**
+   * @todo stocker dans un fichier sur le serveur ou sur le pc local ou laisser comme ca (afficher le json dans la console)
+   */
   const saveAsFile = () => {
     console.log(JSON.stringify(gameOutput()));
   };
+  /**
+   * créer une instance CardClass et la renvoie 
+   *  @param obj - information mimimum pour creer une carte 
+   *               carte simple juste la couleur
+   *               carte complex les 2 cartes qui la compose et la liaison
+   *  @param i - numero de l'id  
+   */
   const toClass = (obj,i) =>{
     if(obj.color === undefined){
       console.log(new CardClass(i,null,false,obj.liaison,toClass(obj.left,0),toClass(obj.right,1)));
