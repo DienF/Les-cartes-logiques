9d3581cf819dd94a2c190cea10cfd2c4463cfa7b
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2022-04-07 21:05:39 +1100

    Petits changements visuels

--- cartes-logiques/src/components/Choice.js
+++ cartes-logiques/src/components/Choice.js
@@ -4,38 +4,35 @@ import { NavLink } from "react-router-dom";
 const Choice = () => {
   return (
     <div className="choice">
-        <ul className="choicebar">
+      <div className="choicebar">
+        <ul>
           <li>
-          <a href="#">Exercices :</a>
-            <ul>
-              <li>
-                <NavLink exact="true" to="/Exercise">
-                  Exercice 1
-                </NavLink>
-              </li>
-              <li>
-                <NavLink exact="true" to="/Exercise">
-                  Exercice 2
-                </NavLink>
-              </li>
-              <li>
-                <NavLink exact="true" to="/Exercise">
-                  Exercice 3
-                </NavLink>
-              </li>
-              <li>
-                <NavLink exact="true" to="/Exercise">
-                  Exercice 4
-                </NavLink>
-              </li>
-              <li>
-                <NavLink exact="true" to="/Exercise">
-                  Exercice 5
-                </NavLink>
-              </li>
-            </ul>
+            <NavLink exact="true" to="/Exercise">
+              Exercice 1
+            </NavLink>
           </li>
-      </ul>
+          <li>
+            <NavLink exact="true" to="/Exercise">
+              Exercice 2
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact="true" to="/Exercise">
+              Exercice 3
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact="true" to="/Exercise">
+              Exercice 4
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact="true" to="/Exercise">
+              Exercice 5
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
     
   );
