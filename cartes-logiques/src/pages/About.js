794b76959f0b3e9e537920f4a199298bfcc9a73e
Adrien Féré <adrien.fere@etudiant.unc.nc>
2022-05-02 19:39:53 +1100

    Symboles des boutons affichés

--- cartes-logiques/src/pages/About.js
+++ cartes-logiques/src/pages/About.js
@@ -7,6 +7,7 @@ const About = () => {
       <Navigation />
       <div className="about"> 
         <h2>À propos de ce site</h2>
+        <img src={"img/logo_unc.png"} alt="Logo de l'Université de Nouvelle-Calédonie"></img>
         <p> Projet tutoré : Création d'un jeu de logique encadré par M. Eric Edo</p>
         <p> Étudiants : </p>
         <ul> 
