096de6ed1e39108ecb0b137c5c5591486ee0eebe
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-03 10:22:22 +1100

    update boot

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 for s in $( screen -ls | grep -o -P "\d\d\d\d\d*"); do screen -X -S $s quit;done;
 git reset --hard
 git pull
+cd react
+npm run build
+mv build/static ../flask
+sed 's/\/static/static/g' build/index.html > ../flask/templates/index.html
+mv build/*.json ../flask/templates
+mv build/* ../flask/static
+cd ..
 cd flask
 screen -d -m sudo python server.py
-cd ..
-cd react
-screen -d -m sudo PORT=80 npm start
