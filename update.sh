c75ed82cb8515d5536f84b7bf1912f1ae0825d76
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-03 10:30:15 +1100

    changement port et boot

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 for s in $( screen -ls | grep -o -P "\d\d\d\d\d*"); do screen -X -S $s quit;done;
 git reset --hard
 git pull
+rm -R flask/static
+rm -R flask/templates/*
 cd react
 npm run build
-mv build/static ../flask
+cp -R build/static ../flask
 sed 's/\/static/static/g' build/index.html > ../flask/templates/index.html
-mv build/*.json ../flask/templates
-mv build/* ../flask/static
+cp -R build/*.json ../flask/templates
+cp -R build/* ../flask/static
+rm -r build/*
 cd ..
 cd flask
 screen -d -m sudo python server.py
