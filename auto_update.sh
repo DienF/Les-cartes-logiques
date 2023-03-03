6dc98b3fe05daa4e1c651b57ae91306eb3b1f90c
Florian-Audouard <nairolf.audouard@gmail.com>
2023-03-03 16:16:45 +1100

    up

--- auto_update.sh
+++ auto_update.sh
@@ -3,16 +3,18 @@ do
     git fetch > /dev/null
     output_git=$(git status -uno)
     ouput_screen=$(screen -ls)
-    if [[$output_git == *""* || $ouput_screen == *"found"*]]
+    echo output_git > /home/ubuntu/output_git.log
+    echo output_screen > /home/ubuntu/output_screen.log
+    if [[$output_git == *""* || $ouput_screen == *"server"*]]
     then 
-        killall screen
+        screen -X -S server quit
         git reset --hard
         git pull
         cd react
         rm -r build
         npm run build
         cd ../flask
-        screen -d -m sudo python server.py
+        screen -S server -d -m sudo python server.py
         cd ..
     fi
     sleep 5
