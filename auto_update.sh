c231fef90488fdb56634a44b01ddb8ef1cf284ff
Florian-Audouard <nairolf.audouard@gmail.com>
2023-03-03 16:58:09 +1100

    up boot

--- auto_update.sh
+++ auto_update.sh
@@ -1,11 +1,10 @@
+cd /home/ubuntu/Les-cartes-logiques
 while [ 1 ]
 do
     git fetch > /dev/null
     output_git=$(git status -uno)
-    ouput_screen=$(screen -ls)
-    echo output_git > /home/ubuntu/output_git.log
-    echo output_screen > /home/ubuntu/output_screen.log
-    if [[$output_git == *""* || $ouput_screen == *"server"*]]
+    output_ps=$(ps -a)
+    if [[ $output_git == *"behind"* || $output_ps != *"python"* ]]
     then 
         screen -X -S server quit
         git reset --hard
