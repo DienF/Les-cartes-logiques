d532749ddca5a963a18e0e85979270fb54771b7e
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-15 18:49:49 +1100

    supression des fichiers sources visible depuis le navigateur

--- auto_update.sh
+++ auto_update.sh
@@ -13,6 +13,9 @@ do
         rm -r build
         npm install
         npm run build
+        rm -r ../flask/build
+        cp -rp build ../flask/build
+        rm -r build
         cd ../flask
         screen -S server -d -m sudo python server.py
     fi
