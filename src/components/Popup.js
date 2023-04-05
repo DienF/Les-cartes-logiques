d7e3b406208c553b79c851b6da17589cd4fd16d9
Adrien Féré <adrien.fere@etudiant.unc.nc>
2023-04-05 16:54:38 +1100

    CSS Page Home
    J'ai enlevé la barre de scroll en bas de la page d'accueil.

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Popup = props => {
-  return (
-    <div className="popup-box">
-      <div className="bigbox" style={{width : props.size + "%"}}>
-        <div className="box">
-          {props.content}
+    return (
+        <div className="popup-box">
+            <div className="bigbox" style={{width : props.size + "%"}}>
+                <div className="box">
+                    {props.content}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Popup;
--- src/components/PopupForms.js
+++ src/components/PopupForms.js
@@ -2,17 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const PopupForms = props => {
-  return (
-    <div className="popupForms-box">
-      <div className="bigbox" style={{width : props.size + "%"}}>
-        <div className="box">
-              <NavLink exact="true" to="/Forms">
-								Votre avis nous intéresse
-							</NavLink>
+    return (
+        <div className="popupForms-box">
+            <div className="bigbox" style={{width : props.size + "%"}}>
+                <div className="box">
+                    <NavLink exact="true" to="/Forms">
+                        Votre avis nous intéresse
+                    </NavLink>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PopupForms;
