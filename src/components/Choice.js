760e96d9b9e0e0bff619b7b97eb3355cebff0f0d
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-04-20 16:20:28 +1100

    changement de choices

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Choice = () => {
 	const jsonCount = 35;
@@ -7,47 +7,42 @@ const Choice = () => {
 	for (let i = 1; i <= jsonCount; i++) {
 		alljson.push(i);
 	}
-	const rownumber = alljson.length/5;
+	const rownumber = alljson.length / 5;
+	const navigate = useNavigate();
+
+	function goToExo(event) {
+		const url = event.target.getAttribute("url");
+		navigate(url);
+	}
 
 	const createRow = (start, end) => {
 		let row = [];
 		for (let index = start; index < end; index++) {
-			row.push(<td key={index}>
-					<NavLink
-						exact="true"
-						to={"/Exercise-Play-" + (index + 1)}
-					>
+			row.push(
+				<td
+					key={index}
+					onClick={goToExo}
+					url={"/Exercise-Play-" + (index + 1)}
+				>
+					<p url={"/Exercise-Play-" + (index + 1)}>
 						Niveau {index + 1}
-					</NavLink>
-				</td>)
+					</p>
+				</td>
+			);
 		}
-		return row
+		return <tr>{row}</tr>;
+	};
+	function AfficheChoice(props) {
+		const res = [];
+		for (let i = 0; i < jsonCount; i += 5) {
+			res.push(createRow(i, i + 5));
+		}
+		return res;
 	}
-
 	return (
 		<div className="choice">
 			<table>
-				<tr>
-					{createRow(0,5)}
-				</tr>
-				<tr>
-					{createRow(5,10)}
-				</tr>
-				<tr>
-					{createRow(10,15)}
-				</tr>
-				<tr>
-					{createRow(15,20)}
-				</tr>
-				<tr>
-					{createRow(20,25)}
-				</tr>
-				<tr>
-					{createRow(25,30)}
-				</tr>
-				<tr>
-					{createRow(30,35)}
-				</tr>
+				<AfficheChoice></AfficheChoice>
 			</table>
 		</div>
 	);
