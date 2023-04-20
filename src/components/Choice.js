7616d581ef651e12b542c6838d238119e7492da4
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2023-04-20 14:49:26 +1100

    modifs composant Choice (js et css)

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -7,23 +7,48 @@ const Choice = () => {
 	for (let i = 1; i <= jsonCount; i++) {
 		alljson.push(i);
 	}
+	const rownumber = alljson.length/5;
+
+	const createRow = (start, end) => {
+		let row = [];
+		for (let index = start; index < end; index++) {
+			row.push(<td key={index}>
+					<NavLink
+						exact="true"
+						to={"/Exercise-Play-" + (index + 1)}
+					>
+						Niveau {index + 1}
+					</NavLink>
+				</td>)
+		}
+		return row
+	}
 
 	return (
 		<div className="choice">
-			<div className="choicebar">
-				<ul>
-					{alljson.map((json, index) => (
-						<li key={index}>
-							<NavLink
-								exact="true"
-								to={"/Exercise-Play-" + (index + 1)}
-							>
-								Niveau {index + 1}
-							</NavLink>
-						</li>
-					))}
-				</ul>
-			</div>
+			<table>
+				<tr>
+					{createRow(0,5)}
+				</tr>
+				<tr>
+					{createRow(5,10)}
+				</tr>
+				<tr>
+					{createRow(10,15)}
+				</tr>
+				<tr>
+					{createRow(15,20)}
+				</tr>
+				<tr>
+					{createRow(20,25)}
+				</tr>
+				<tr>
+					{createRow(25,30)}
+				</tr>
+				<tr>
+					{createRow(30,35)}
+				</tr>
+			</table>
 		</div>
 	);
 };
