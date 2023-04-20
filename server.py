ba52facaa7e6e6c59ae7b9725ee6e7e19b7b11e1
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-04-20 14:49:29 +1100

    test vercel

--- server.py
+++ server.py
@@ -3,30 +3,10 @@ Server Flask avec react
 """
 import os
 
-import urllib.parse
-
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+from database import get_database
 
-from dotenv import dotenv_values
-import psycopg
-
-os.chdir(os.path.dirname(__file__))
-
-if os.path.exists(".env"):
-    config = dotenv_values(".env")
-else:
-    config = dotenv_values("default.env")
-
-FILENAME_DB_SHEMA = "data.sql"
-options = urllib.parse.quote_plus("--search_path=modern,public")
-CONN_PARAMS = f"postgresql://{config['USER']}:{config['PASSWORD']}@{config['HOST']}:{config['PORT']}/{config['DATABASE']}?options={options}"  # pylint: disable=line-too-long
-
-
-with psycopg.connect(CONN_PARAMS) as conn:  # pylint: disable=not-context-manager
-    with conn.cursor() as cur:
-        with open(FILENAME_DB_SHEMA, "r", encoding="utf-8") as file:
-            cur.execute(file.read())
 
 app = Flask(__name__, static_folder="./build")
 CORS(app, origins="http://localhost:3000")
@@ -38,11 +18,8 @@ def test():  # pylint: disable=missing-function-docstring
 
 
 @app.route("/getDatabase", methods=["GET"])
-def get_database():  # pylint: disable=missing-function-docstring
-    with psycopg.connect(CONN_PARAMS) as conn:  # pylint: disable=not-context-manager
-        with conn.cursor() as cur:
-            cur.execute("select * from data_carte;")
-            return jsonify(cur.fetchall())
+def get_database_server():  # pylint: disable=missing-function-docstring
+    return jsonify(get_database())
 
 
 @app.route("/", defaults={"path": ""})
