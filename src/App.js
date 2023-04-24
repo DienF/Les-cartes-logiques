737c96584dc0caad6ee2f820e66d36503d1e4cff
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-04-24 16:00:40 +1100

    résolution warning

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ import Levels from "./pages/Levels";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
-	let url_add = "";
-	if (process.env.NODE_ENV === "development") url_add = "http://localhost:80";
-	fetch(url_add + "/getDatabase")
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data[0][1]);
-		});
-	fetch(url_add + "/test")
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data.test);
-		});
+	// let url_add = "";
+	// if (process.env.NODE_ENV === "development") url_add = "http://localhost:80";
+	// fetch(url_add + "/getDatabase")
+	// 	.then((res) => res.json())
+	// 	.then((data) => {
+	// 		console.log(data[0][1]);
+	// 	});
+	// fetch(url_add + "/test")
+	// 	.then((res) => res.json())
+	// 	.then((data) => {
+	// 		console.log(data.test);
+	// 	});
 	return (
 		<BrowserRouter>
 			<Routes>
--- src/components/Choice.js
+++ src/components/Choice.js
@@ -3,7 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 const Choice = () => {
 	const jsonCount = 36;
-	const difficulty = [[1, 20, "Démonstrations"], [21, 35," Raisonnements"], [36, 40, "Autres"]];
+	const difficulty = [
+		[1, 20, "Démonstrations"],
+		[21, 35, " Raisonnements"],
+		[36, 40, "Autres"],
+	];
 	const navigate = useNavigate();
 
 	function goToExo(event) {
@@ -16,34 +20,46 @@ const Choice = () => {
 		for (let index = start; index < end; index++) {
 			if (index < jsonCount) {
 				row.push(
-					<td key={index} onClick={goToExo} url={"/Exercise-Play-" + (index + 1)}>
+					<td
+						key={index}
+						onClick={goToExo}
+						url={"/Exercise-Play-" + (index + 1)}
+					>
 						<p url={"/Exercise-Play-" + (index + 1)}>
 							Niveau {index + 1}
 						</p>
 					</td>
 				);
-			}
-			else row.push(<td key={index} className="tdVoid"><p>Niveau XX</p></td>);
+			} else
+				row.push(
+					<td key={index} className="tdVoid">
+						<p>Niveau XX</p>
+					</td>
+				);
 		}
-		return <tr key={start+"-"+end}>{row}</tr>;
+		return <tr key={start + "-" + end}>{row}</tr>;
 	};
 
 	function AfficheChoice(props) {
 		const res = [];
-		difficulty.forEach(category => {
+		difficulty.forEach((category, index) => {
 			let table = [];
 			let y = 1;
-			res.push(<h2>{category[2]}</h2>);
-			for (let i = category[0]-1; i <= category[1]-1; i ++) {
-				if (i+5 >= category[1] && y === 1){
+			res.push(<h2 key={"h-" + index}>{category[2]}</h2>);
+			for (let i = category[0] - 1; i <= category[1] - 1; i++) {
+				if (i + 5 >= category[1] && y === 1) {
 					table.push(createRow(i, category[1]));
-					y = 5
+					y = 5;
 				} else if (i < category[1] && y === 1) {
-					table.push(createRow(i, i+5));
-					y = 5
+					table.push(createRow(i, i + 5));
+					y = 5;
 				} else y--;
 			}
-			res.push(<table key={category[2]}><tbody>{table}</tbody></table>);
+			res.push(
+				<table key={category[2]}>
+					<tbody>{table}</tbody>
+				</table>
+			);
 		});
 		return res;
 	}
