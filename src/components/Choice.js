891a9dd04fdb6d792219b6184717378f46cbeed2
Perron Guillaume <guillaume.perron@etudiant.unc.nc>
2023-04-24 14:56:05 +1100

    Refonte fonction AfficheChoice et CSS

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const Choice = () => {
 	const jsonCount = 36;
-	const Difficulty = [[1, 20, "Démonstrations"], [21, 35," Raisonnements"], [36, 37, "Autres"]];
+	const difficulty = [[1, 20, "Démonstrations"], [21, 35," Raisonnements"], [36, 40, "Autres"]];
 	const navigate = useNavigate();
 
 	function goToExo(event) {
@@ -16,34 +16,34 @@ const Choice = () => {
 		for (let index = start; index < end; index++) {
 			if (index < jsonCount) {
 				row.push(
-					<td
-						key={index}
-						onClick={goToExo}
-						url={"/Exercise-Play-" + (index + 1)}
-					>
+					<td key={index} onClick={goToExo} url={"/Exercise-Play-" + (index + 1)}>
 						<p url={"/Exercise-Play-" + (index + 1)}>
 							Niveau {index + 1}
 						</p>
 					</td>
 				);
 			}
-			else row.push(<td key={index}><p>   </p></td>);
+			else row.push(<td key={index} className="tdVoid"><p>Niveau XX</p></td>);
 		}
-		return <tr>{row}</tr>;
+		return <tr key={start+"-"+end}>{row}</tr>;
 	};
 
 	function AfficheChoice(props) {
 		const res = [];
-		Difficulty.forEach(category => {
+		difficulty.forEach(category => {
 			let table = [];
+			let y = 1;
 			res.push(<h2>{category[2]}</h2>);
-			console.log(category[0]);
-			console.log(category[1]);
-			res.push(<table>{table}</table>);
-			for (let i = category[0]-1; i < category[1]-1; i += 5) {
-				if (i > category[1]) res.push(table.push(<table>{createRow(i, category[1])}</table>))
-				else                 table.push(createRow(i, i + 5));
+			for (let i = category[0]-1; i <= category[1]-1; i ++) {
+				if (i+5 >= category[1] && y === 1){
+					table.push(createRow(i, category[1]));
+					y = 5
+				} else if (i < category[1] && y === 1) {
+					table.push(createRow(i, i+5));
+					y = 5
+				} else y--;
 			}
+			res.push(<table key={category[2]}><tbody>{table}</tbody></table>);
 		});
 		return res;
 	}
